887a15b11edd19770e33a4b2c0545b2c54b5c157
carlosturnerbenites <carlosturnerbenites@gmail.com>
2016-12-06 16:16:14 -0500

    Añadiendo Template Base

---
+++ src/AppBundle/Controller/CmsStzefTypesAccessController.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\CmsStzefTypesAccess;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Cmsstzeftypesaccess controller.
+ *
+ * @Route("types_access")
+ */
+class CmsStzefTypesAccessController extends Controller
+{
+    /**
+     * Lists all cmsStzefTypesAccess entities.
+     *
+     * @Route("/", name="types_access_index")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $cmsStzefTypesAccesses = $em->getRepository('AppBundle:CmsStzefTypesAccess')->findAll();
+
+        return $this->render('cmsstzeftypesaccess/index.html.twig', array(
+            'cmsStzefTypesAccesses' => $cmsStzefTypesAccesses,
+        ));
+    }
+
+    /**
+     * Creates a new cmsStzefTypesAccess entity.
+     *
+     * @Route("/new", name="types_access_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request)
+    {
+        $cmsStzefTypesAccess = new CmsStzefTypesAccess();
+        $form = $this->createForm('AppBundle\Form\CmsStzefTypesAccessType', $cmsStzefTypesAccess);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($cmsStzefTypesAccess);
+            $em->flush($cmsStzefTypesAccess);
+
+            return $this->redirectToRoute('types_access_show', array('id' => $cmsStzefTypesAccess->getId()));
+        }
+
+        return $this->render('cmsstzeftypesaccess/new.html.twig', array(
+            'cmsStzefTypesAccess' => $cmsStzefTypesAccess,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a cmsStzefTypesAccess entity.
+     *
+     * @Route("/{id}", name="types_access_show")
+     * @Method("GET")
+     */
+    public function showAction(CmsStzefTypesAccess $cmsStzefTypesAccess)
+    {
+        $deleteForm = $this->createDeleteForm($cmsStzefTypesAccess);
+
+        return $this->render('cmsstzeftypesaccess/show.html.twig', array(
+            'cmsStzefTypesAccess' => $cmsStzefTypesAccess,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing cmsStzefTypesAccess entity.
+     *
+     * @Route("/{id}/edit", name="types_access_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, CmsStzefTypesAccess $cmsStzefTypesAccess)
+    {
+        $deleteForm = $this->createDeleteForm($cmsStzefTypesAccess);
+        $editForm = $this->createForm('AppBundle\Form\CmsStzefTypesAccessType', $cmsStzefTypesAccess);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('types_access_edit', array('id' => $cmsStzefTypesAccess->getId()));
+        }
+
+        return $this->render('cmsstzeftypesaccess/edit.html.twig', array(
+            'cmsStzefTypesAccess' => $cmsStzefTypesAccess,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a cmsStzefTypesAccess entity.
+     *
+     * @Route("/{id}", name="types_access_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, CmsStzefTypesAccess $cmsStzefTypesAccess)
+    {
+        $form = $this->createDeleteForm($cmsStzefTypesAccess);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($cmsStzefTypesAccess);
+            $em->flush($cmsStzefTypesAccess);
+        }
+
+        return $this->redirectToRoute('types_access_index');
+    }
+
+    /**
+     * Creates a form to delete a cmsStzefTypesAccess entity.
+     *
+     * @param CmsStzefTypesAccess $cmsStzefTypesAccess The cmsStzefTypesAccess entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(CmsStzefTypesAccess $cmsStzefTypesAccess)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('types_access_delete', array('id' => $cmsStzefTypesAccess->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+}
